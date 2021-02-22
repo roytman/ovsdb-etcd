58ab2ecb676b512dff91e52e9d5a157c11c847f8
Alexey Roytman <roytman@il.ibm.com>
2021-02-22 17:52:46 +0200

    Add codegenerator and generated types
    Signed-off-by: Alexey Roytman <roytman@il.ibm.com>

--- pkg/cmd/codegenerator/generator.go
+++ pkg/cmd/codegenerator/generator.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"flag"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"k8s.io/klog"
 
 	"github.com/roytman/ovsdb-etcd/pkg/codegenerator"
 )
@@ -26,12 +29,20 @@ func Execute() error {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+	klog.InitFlags(nil)
+
+	pflag.CommandLine.AddGoFlag(flag.CommandLine.Lookup("v"))
+	pflag.CommandLine.AddGoFlag(flag.CommandLine.Lookup("logtostderr"))
+	pflag.CommandLine.Set("logtostderr", "true")
 
 	rootCmd.PersistentFlags().StringVarP(&codegenerator.SchemaFile, "schemaFile", "s", "", "input schema file")
 	rootCmd.MarkPersistentFlagRequired("schemaFile")
-	rootCmd.PersistentFlags().StringVarP(&codegenerator.OutputDir, "outputDir", "o", ".",  "output directory")
+	rootCmd.PersistentFlags().StringVarP(&codegenerator.OutputFile, "outputFile", "o", "types.go",  "ouput file for the generated code, default is 'types.go'")
+	rootCmd.PersistentFlags().StringVarP(&codegenerator.DestinationDir, "destinationDir", "d", ".",  "base directory to store generated code, default is '.'")
 	rootCmd.PersistentFlags().StringVarP(&codegenerator.PkgName, "packageName", "p", "", "the package of generated files, default is database name")
+	rootCmd.PersistentFlags().StringVarP(&codegenerator.BasePackage, "basePackage", "b", "",  "package with the base structures definitions, default is this 'repository/pkg/json'")
 }
+
 
 func initConfig() {
 
